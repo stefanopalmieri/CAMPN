6f3499e19375e1a74cedc74c426967112d81d841
stefanopalmieri <stefano.r.palmieri@gmail.com>
2016-12-18 13:01:11 -0600

    Began work on innovation numbers
    Also fixed comment style

--- genome.py
+++ genome.py
@@ -4,6 +4,7 @@ import random
 from enum import Enum
 import matplotlib.pyplot as plt
 import tkinter
+from InnovationDB import InnovationDB
 # import scipy.signal
 # import pygraphviz
 # import pydot
@@ -22,12 +23,11 @@ import tkinter
 # Written by Stefano Palmieri in December 2016
 
 
-# This class stores the genome as a weighted directed graph representing
-# a Compositional Adjacency Matrix Producing Network. The weighted directed
-#  graph is produced from Nodes and Links. Nodes can output a constant
-# matrix (tensor) or produce the matrix product, transpose, convolution, or
-# Kronecker product of the input matrices. Links are floating point values
-# that are use for scalar multiplication of matrices outputted from Nodes.
+# This class stores the genome as a weighted Multi-Directed Acyclical
+# Graph called an Compositional Adjacency Matrix Producing Network. The CAMPN
+# graph is produced from Nodes and Links. Nodes can output a constant
+# matrix (tensor) or perform a matrix operation such as Hadamard product.
+
 
 # Enumerate node types
 # If you want to add more node types, add after Hadamard
@@ -43,7 +43,7 @@ class NodeType(Enum):
 class Genome(object):
 
     # Probability of mutating a new node
-    P_NEW_NODE = 0.1
+    P_NEW_NODE = 0.05
 
     # Probability of mutating a new input node
     P_NEW_INPUT_NODE = 0.1
@@ -68,14 +68,23 @@ class Genome(object):
     MUTATE_MEAN = 0.1
     MUTATE_STD = 0.1
 
-    def __init__(self):
+    def __init__(self, innovation_db=None):
+        self.innovation_db = innovation_db
         self.network = nx.MultiDiGraph()
 
-        # node with 0 id is the output
-        self.network.add_node(0, type=NodeType.output)
-        self.network.add_node(1, type=NodeType.input,
-                              constant=np.zeros((self.CONSTANT_ROW_SIZE, self.CONSTANT_COL_SIZE), dtype=np.float16))
-        self.network.add_edge(1, 0, key=True, weight=1.0)
+        # node with id 1 is the output
+        self.network.add_node(1, type=NodeType.output)
+        self.network.add_node(2, type=NodeType.input,
+        					  constant=np.matrix([[0, 1, 0, 0],
+                                                  [0, 0, 0, 0], 
+                                                  [0, 0, 0, 1], 
+                                                  [0, 0, 0, 0]], 
+                                                  dtype=np.float16))
+        self.network.add_edge(2, 1, key=True, weight=1.0, innovation=3)
+
+        self.innovation_db.direct_insert('output', 0, 0)
+        self.innovation_db.direct_insert('input', 0, 0)
+        self.innovation_db.direct_insert('left', 1, 2)
 
     # Mutations can add a Node, add a Link, change the weight of a Link,
     # change the function of a Node.
@@ -111,42 +120,68 @@ class Genome(object):
         # Randomly select a link from the existing network
         start, end, key = random.choice(self.network.edges(keys=True))
 
-        # Randomly select the function for this node but don't include Input or Output types as functions
-        function = NodeType(random.randrange(NodeType.input.value + 1, len(NodeType)))
+        # Randomly select the function for this node but don't include Input
+        # or Output types as functions
+        function = NodeType(random.randrange(NodeType.input.value + 1,
+                            len(NodeType)))
 
         # Create a new node with the function type
-        node_num = self.network.number_of_nodes()
+        node_num = self.innovation_db.retrieve_innovation_num(function.name,
+                                                              start, end)
         self.network.add_node(node_num, type=function)
 
         # Add links for the new node
-        self.network.add_edge(start, node_num, key=random.random() < 0.5, weight=self.N_LINK_WEIGHT)
-        self.network.add_edge(node_num, end, key=key, weight=self.N_LINK_WEIGHT)
+        in_key = random.random() < 0.5
+        in_type = 'left' if incoming_key else 'right'
+        in_innov_num = self.innovation_db.retreive_innovation_num(in_type,
+                                                                  start,
+                                                                  node_num)
+        self.network.add_edge(start, node_num, 
+                              key=in_key, weight=self.N_LINK_WEIGHT)
 
-        # Remove old edge (in future versions this edge should be disabled rather than removed)
+        out_key = random.random() < 0.5
+        out_type = 'left' if outgoing_key else 'right'
+        out_innov_num = self.innovation_db.retrieve_innovation_num(out_type,
+                                                                   start,
+                                                                   node_num)
+        self.network.add_edge(node_num, end,
+                              key=key, weight=self.N_LINK_WEIGHT)
+
+        # Remove old edge (in future versions this edge should
+        # be disabled rather than removed)
         self.network.remove_edge(start, end, key=key)
 
-    # Create a new random Node by selecting a non-input node and creating a child
+    # Create a new random input Node by selecting a non-input node
+    # and creating a child
     def mutate_new_input_node(self):
 
         # Get list of non-input nodes
-        non_input_nodes = [node for node, data in self.network.nodes(data=True) if data['type'] != NodeType.input]
+        non_input_nodes = [node for node, data \
+                          in self.network.nodes(data=True) \
+                          if data['type'] != NodeType.input]
 
         # randomly pick a node from the list
         node = random.choice(non_input_nodes)
 
         # Create a new input node
-        node_num = self.network.number_of_nodes()
+        node_num = self.network.number_of_nodes() + 1
         self.network.add_node(node_num, type=NodeType.input,
-                              constant=np.zeros((self.CONSTANT_ROW_SIZE, self.CONSTANT_COL_SIZE), dtype=np.float16))
+                              constant=np.zeros((self.CONSTANT_ROW_SIZE,
+                                                 self.CONSTANT_COL_SIZE),
+                                                dtype=np.float16))
 
         # Create a link from the new input node to existing non-input node
-        self.network.add_edge(node_num, node, key=random.random() < 0.5, weight=self.N_LINK_WEIGHT)
+        self.network.add_edge(node_num, node, 
+                              key=random.random() < 0.5,
+                              weight=self.N_LINK_WEIGHT)
 
     # Randomly mutates one of the existing input nodes in the CAMPN.
     def mutate_node(self):
 
         # get list of input nodes
-        input_nodes = [node for node, data in self.network.nodes(data=True) if data['type'] == NodeType.input]
+        input_nodes = [node for node, data \
+                      in self.network.nodes(data=True) \
+                      if data['type'] == NodeType.input]
 
         # randomly pick a node from the list
         node = random.choice(input_nodes)
@@ -163,12 +198,17 @@ class Genome(object):
 
     # Create a new Link and add it to the Genome.
     def mutate_new_link(self):
-        # select a random node to start a link from and, don't include output node
-        list = [node for node, data in self.network.nodes(data=True) if data['type'] != NodeType.output]
+        # select a random node to start a link from and
+        # don't include output node
+        list = [node for node, data \
+               in self.network.nodes(data=True) \
+               if data['type'] != NodeType.output]
         start = random.choice(list)
 
         # select a random node to end link at, don't include input nodes
-        list = [node for node, data in self.network.nodes(data=True) if data['type'] != NodeType.input and node != start]
+        list = [node for node, data \
+               in self.network.nodes(data=True) \
+               if data['type'] != NodeType.input and node != start]
         end = random.choice(list)
 
         # Create the link
@@ -198,7 +238,7 @@ class Genome(object):
     def get_phenotype(self, node=None):
 
         if node is None:
-            return self.get_phenotype(0)
+            return self.get_phenotype(1)
 
         if self.network.node[node]['type'] == NodeType.input:
             return self.network.node[node]['constant']
@@ -210,7 +250,8 @@ class Genome(object):
 
     def calculate_logit(self, node, key):
         predecessors = list(self.network.predecessors(node))
-        nodes = [predecessor for predecessor in predecessors if key in self.network[predecessor][node]]
+        nodes = [predecessor for predecessor \
+                in predecessors if key in self.network[predecessor][node]]
 
         if nodes:
             logit = np.zeros((self.CONSTANT_ROW_SIZE, self.CONSTANT_COL_SIZE))
@@ -234,7 +275,8 @@ class Genome(object):
             elif right_logit is None:
                 return left_logit
             else:
-                left_logit, right_logit = self.same_size(left_logit, right_logit)
+                left_logit, right_logit = self.same_size(left_logit, 
+                                                         right_logit)
                 return np.add(left_logit, right_logit)
 
         elif self.network.node[node]['type'] == NodeType.input:
@@ -256,7 +298,8 @@ class Genome(object):
             elif right_logit is None:
                 return left_logit
             else:
-                left_logit, right_logit = self.same_size(left_logit, right_logit)
+                left_logit, right_logit = self.same_size(left_logit,
+                                                         right_logit)
                 return np.multiply(left_logit, right_logit)
 
     # returns left and right so that they are the same size, assuming both are square
@@ -276,9 +319,10 @@ class Genome(object):
 
 # Testing Genotype stuff
 
-genome = Genome()
+innovation_db = InnovationDB()
+genome = Genome(innovation_db)
 
-for i in range(0, 40):
+for i in range(0, 20):
     genome.mutate()
 
 print("printing nodes")
@@ -313,4 +357,72 @@ print(phenotype)
 print(np.size(phenotype, 0))
 
 plt.matshow(phenotype, fignum=100, cmap=plt.cm.gray)
+plt.show()
+
+def unpart1by1(n):
+    n &= 0x55555555
+    n = (n ^ (n >> 1)) & 0x33333333
+    n = (n ^ (n >> 2)) & 0x0f0f0f0f
+    n = (n ^ (n >> 4)) & 0x00ff00ff
+    n = (n ^ (n >> 8)) & 0x0000ffff
+    return n
+
+
+def deinterleave2(n):
+    return unpart1by1(n >> 1), unpart1by1(n)
+
+
+# Creates a two-dimensional
+# #Z-order curve by using the de-interleaving method
+def z_layout(n):
+    pos = {}
+    for i in range(0, n):
+        pos[i] = deinterleave2(i)
+    return pos
+
+def twirl(n):
+    mask = 0x80000000
+
+    for i in range(0, 15):
+        n = n ^ ((n & (mask >> (2 * i + 1))) >> 1)
+        n = n ^ ((n & (mask >> (2 * i))) >> 2)
+
+    return n
+
+# Creates a Symmetrical Z-order curve
+def sym_z_layout(n):
+    pos = {}
+    for i in range(0, n):
+        pos[i] = deinterleave2(twirl(i))
+    return pos
+
+# Parameters for drawing graphs
+node_size=100
+node_color='blue'
+node_alpha=0.3
+node_text_size=12
+edge_color='blue'
+edge_alpha=0.3
+edge_thickness=1
+edge_text_pos=0.8
+text_font='sans-serif'
+
+phenotype[phenotype < 0.1] = 0
+
+G = nx.from_numpy_matrix(phenotype, create_using=nx.DiGraph())
+
+pos = sym_z_layout(np.ma.size(phenotype, 0))
+
+colorList = []
+for j in G.edges():
+    a, b = j
+    colorVal = G.edge[a][b]['weight']
+    colorList.append(colorVal)
+
+ # nx.draw_networkx(G[i], pos=pos[i], ax=axes[i])
+nx.draw_networkx_nodes(G, pos, node_size=node_size,
+                           alpha=node_alpha, node_color=node_color)
+nx.draw_networkx_edges(G, pos, width=1, edge_color=colorList, edge_cmap=plt.cm.RdYlGn, arrows=True)
+nx.draw_networkx_labels(G, pos, font_size=node_text_size,
+                            font_family=text_font)
 plt.show()
